0cbd5a9f85bc80b2740e16738c986f93910a79a9
picalfer <trable228@icloud.com>
2022-08-20 01:21:51 +0300

    async with Handler (deprecated)

--- app/src/main/java/com/example/coroutine_test/MainActivity.kt
+++ app/src/main/java/com/example/coroutine_test/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.coroutine_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.coroutine_test.databinding.ActivityMainBinding
@@ -10,6 +12,8 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
+        Looper.prepare()
+        Handler() // only after Looper.prepare() we can use Handler constructor on no main thread
+
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
 
@@ -38,20 +45,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                "Loading temperature for city: $city",
-                Toast.LENGTH_SHORT
-            ).show()
+            handler.post {
+                Toast.makeText(
+                    this,
+                    "Loading temperature for city: $city",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-            callback.invoke(17)
+            handler.post {
+                callback.invoke(17)
+            }
         }
     }
 
     private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            handler.post {
+                callback.invoke("Moscow")
+            }
         }
     }
 
